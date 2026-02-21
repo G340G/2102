1ecb08bc8219995dd7bd25e3beeb3b9518821a4b
scarbo <58745420+G340G@users.noreply.github.com>
2026-02-21 22:03:48 +0100

    Add choose_style function to select a style randomly

---
+++ director.py
@@ -0,0 +1,10 @@
+import random
+
+def choose_style():
+    return random.choice([
+        "EDUCATIONAL TAPE",
+        "EMERGENCY ALERT",
+        "INTERNAL TRAINING",
+        "RELIGIOUS WARNING",
+        "CONTAINMENT FAILURE"
+    ])
